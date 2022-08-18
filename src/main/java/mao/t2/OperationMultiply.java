0a376f33803d550da34d7ebbcbe5a776d1c79b0b
mao <1296193245@qq.com>
2022-08-18 20:29:18 +0800

    java_design_patterns_strategy_mode

---
+++ src/main/java/mao/t2/OperationMultiply.java
@@ -0,0 +1,25 @@
+package mao.t2;
+
+/**
+ * Project name(项目名称)：java设计模式_策略模式
+ * Package(包名): mao.t2
+ * Class(类名): OperationMultiply
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/18
+ * Time(创建时间)： 20:27
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class OperationMultiply implements Strategy
+{
+
+    @Override
+    public int doOperation(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+}
---
+++ src/main/java/mao/t2/Strategy.java
@@ -0,0 +1,19 @@
+package mao.t2;
+
+/**
+ * Project name(项目名称)：java设计模式_策略模式
+ * Package(包名): mao.t2
+ * Interface(接口名): Strategy
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/18
+ * Time(创建时间)： 20:25
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public interface Strategy
+{
+    int doOperation(int num1, int num2);
+}
